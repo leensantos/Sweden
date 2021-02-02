7cfa8aa60615f8187d86ea19e5ea0bca7e4c99ba
leensantos <me4299@gmail.com>
2021-02-02 16:24:41 +0800

    nav?

--- index.php
+++ index.php
@@ -7,11 +7,24 @@
 
 <head>
   <link href="" rel="icon" sizes="16x16">
-  <link href="css/styles.css" rel="stylesheet" type="text/css">
+  <link rel="stylesheet" href="css/styles.css">
   <title>Home | The Grand Sweden Hotel</title>
 </head>
 
 <body>
+  <nav>
+    <div class="logo">Sweden Logo</div>
+    <ul>
+      <li><a href="index.php">Home</a></li>
+      <li><a href="Accommodations.php">Accommodations</a></li>
+      <li><a href="Experiences.php">Experiences</a></li>
+      <li><a href="Amenities.php">Amenities</a></li>
+      <li><a href="Offers.php">Offers</a></li>
+      <li><a href="Gallery.php">Gallery</a></li>
+      <li><a href="Destination.php">Destination</a></li>
+      <li><a href="ContactUs.php">Contact Us</a></li>
+    </ul>
+  </nav>
 </body>
 
 </html>
