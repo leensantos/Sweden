173dba568031cd7bd9a31b2df273061fb0ffcaec
leensantos <me4299@gmail.com>
2021-02-03 19:31:43 +0800

    footer stc..

--- index.php
+++ index.php
@@ -57,23 +57,38 @@
       </p>
     </div>
   </section>
-  <footer class="grid">
-    <div>
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque temporibus suscipit repellendus illum cupiditate?
-      Eligendi, esse accusantium laborum a enim, ad excepturi voluptas officia soluta dolores veniam quaerat molestias
-      quia?
-    </div>
-    <div>
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque temporibus suscipit repellendus illum cupiditate?
-      Eligendi, esse accusantium laborum a enim, ad excepturi voluptas officia soluta dolores veniam quaerat molestias
-      quia?
-    </div>
-    <div>
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque temporibus suscipit repellendus illum cupiditate?
-      Eligendi, esse accusantium laborum a enim, ad excepturi voluptas officia soluta dolores veniam quaerat molestias
-      quia?
-    </div>
-
+  <footer>
+    <div class="grid-three">
+      <div>
+        <p>The Grand Sweden Hotel</p>
+        <a href="index.php">Home</a><br />
+        <a href="Experiences.php">Experiences</a><br />
+        <a href="Accommodations.php">Accommodations</a><br />
+        <a href="Amenities.php">Amenities</a><br />
+        <a href="Offers.php">Offers</a><br />
+        <a href="Gallery.php">Gallery</a><br />
+        <a href="Destination.php">Destination</a><br />
+        <a href="ContactUs.php">Contact Us</a><br />
+        <a href="BookNow.php" class="btn">Book Now</a>
+      </div>
+      <div class="footer-address">
+        <h3>Get in touch</h3>
+        <h4>PO Box 222<br />
+          333 Sen. Gil J. Puyat Ave, Makati <br />
+          admissions@mapua.edu.ph <br />
+          +632 8247-5000
+        </h4>
+        <h4>Subject to change<br />
+        </h4>
+      </div>
+      <div>
+        <h3>Find Us On</h3>
+        <img src="assets/logo/facebook.png" alt="facebook" width="50" height="50">&nbsp&nbsp
+        <img src="assets/logo/instagram.png" alt="instagram" width="50" height="50">&nbsp&nbsp
+        <img src="assets/logo/twitter.png" alt="twitter" width="50" height="50">
+      </div>
+    </div><br /><br />
+    <div class="center">© Group Sweden 2021</div>
   </footer>
 </body>
 
