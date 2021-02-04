177e558d58a5c1730eafefd6d8fee4ee40bb3eaa
cjariola <ariola.cydel@gmail.com>
2021-02-04 20:29:03 +0800

    this is for footer

--- Gallery.php
+++ Gallery.php
@@ -2,10 +2,68 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/footer.css">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
     <h1> hello world </h1>
+    <h2> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio quasi vel animi maxime nulla inventore nostrum culpa illum! Odio perferendis labore maxime quidem nesciunt eius temporibus exercitationem tempore, at mollitia. </h2>
+    <br>
+    <h2> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio quasi vel animi maxime nulla inventore nostrum culpa illum! Odio perferendis labore maxime quidem nesciunt eius temporibus exercitationem tempore, at mollitia. </h2>
+    <br>
+    <h2> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio quasi vel animi maxime nulla inventore nostrum culpa illum! Odio perferendis labore maxime quidem nesciunt eius temporibus exercitationem tempore, at mollitia. </h2>
+    <br>
+    <h2> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio quasi vel animi maxime nulla inventore nostrum culpa illum! Odio perferendis labore maxime quidem nesciunt eius temporibus exercitationem tempore, at mollitia. </h2>
+    <br>
+    <h2> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio quasi vel animi maxime nulla inventore nostrum culpa illum! Odio perferendis labore maxime quidem nesciunt eius temporibus exercitationem tempore, at mollitia. </h2>
+    <br>
+    <h2> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio quasi vel animi maxime nulla inventore nostrum culpa illum! Odio perferendis labore maxime quidem nesciunt eius temporibus exercitationem tempore, at mollitia. </h2>
+    <br>
+    <br><br><br><br><br><br><br>
+    <footer style="background-color:black;">
+    <br><br><br>    
+    <div class="container">
+  	 	<div class="row">
+  	 		<div class="footer-col">
+  	 			<h4>Grand Sweden Hotel</h4>
+  	 			<ul>
+  	 				<li><a href="#">Home</a></li>
+  	 				<li><a href="#">Accomodation</a></li>
+  	 				<li><a href="#">Experiences</a></li>
+  	 				<li><a href="#">Amenities</a></li>
+                    <li><a href="#">Offers</a></li>
+  	 				<li><a href="#">Gallery</a></li>
+  	 				<li><a href="#">Destination</a></li>
+  	 				<li><a href="#">Contact Us</a></li>
+  	 			</ul>
+  	 		</div>
+  	 		<div class="footer-col">
+  	 			<h4>Get in touch</h4>
+  	 			<ul>
+  	 				<li><a href="#">PO Box 222 333 Sen. Gil J. Puyat Ave, Makati</a></li>
+  	 				<li><a href="#">+632 8247-5000</a></li>
+  	 				<li><a href="#">Privacy Policy</a></li>
+  	 				<li><a href="#">dress</a></li>
+  	 			</ul>
+  	 		</div>
+  	 		<div class="footer-col">
+  	 			<h4>follow us</h4>
+  	 			<div class="social-links">
+  	 				<a href="#"><i class="fab fa-facebook-f"></i></a>
+  	 				<a href="#"><i class="fab fa-twitter"></i></a>
+  	 				<a href="#"><i class="fab fa-instagram"></i></a>
+  	 				<a href="#"><i class="fab fa-youtube"></i></a>
+  	 			</div>
+  	 		</div>
+  	 	</div>
+           <center>
+           <br>
+           <a style="color: #bbbbbb;">© Copyright 2021. All Rights Reserved.</a>
+            </center>
+       </div>
+       <br>   
+    </footer>
 </body>
-</html>
+</html> 
