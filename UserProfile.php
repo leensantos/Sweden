3e87fafa5205086dc63a2670e58ff2f4e5d15bd9
KrystelD <123krystel123@gmail.com>
2021-02-04 21:20:20 +0800

    head

--- UserProfile.php
+++ UserProfile.php
@@ -1,10 +1,17 @@
 <?php
 ?>
 
+<?php
+
+?>
+
 <!DOCTYPE html>
 <html>
 
 <head>
+  <link href="" rel="icon" sizes="16x16">
+  <link href="css/styles.css" rel="stylesheet" type="text/css">
+  <title>User Profile | The Grand Sweden Hotel</title>
 </head>
 
 <body>
