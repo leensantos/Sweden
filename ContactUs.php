5fe15004898cf449196f77dadab1d32d8cf8a9ac
leensantos <me4299@gmail.com>
2021-02-07 02:40:36 +0800

    sign in, sign up, contact us tbc

--- ContactUs.php
+++ ContactUs.php
@@ -1,17 +1,47 @@
-<?php
-
-?>
-
 <!DOCTYPE html>
 <html>
 
 <head>
-  <link href="" rel="icon" sizes="16x16">
-  <link href="css/styles.css" rel="stylesheet" type="text/css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="css/styles.css">
   <title>Contact Us | The Grand Sweden Hotel</title>
 </head>
 
 <body>
+  <div class="cont">
+    <form action="checklogin.php" method="POST" class="form sign-in" id="contact-us">
+      <h2>Got any Questions?</h2>
+      <label>
+        <span>Email Address</span>
+        <input type="email" name="email">
+      </label>
+      <label>
+        <span>Message</span>
+        <textarea rows="4" cols="35" name="comment" form="contact-us">
+</textarea>
+      </label>
+      <button class="submit img-btn" type="button">Submit</button>
+
+    </form>
+
+    <div class="sub-cont">
+      <div class="img">
+        <div class="img-text m-up">
+          <h2>Contact Us</h2>
+          <p>Sign up and enjoy the best hotel rewards & exclusive membership offers!</p>
+        </div>
+        <div class="img-text m-in">
+          <h2></h2>
+          <p></p>
+        </div>
+      </div>
+      <p class="form sign-up">
+        Message Sent!
+      </p>
+    </div>
+  </div>
+  <script type="text/javascript" src="js/sign-in-script.js"></script>
 </body>
 
 </html>
