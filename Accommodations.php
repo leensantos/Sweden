09160d4fa9c1f3e9ba90b52147b83e9a23648d80
KrystelD <123krystel123@gmail.com>
2021-02-06 09:35:02 +0800

    raw

--- Accommodations.php
+++ Accommodations.php
@@ -7,11 +7,224 @@
 
 <head>
   <link href="" rel="icon" sizes="16x16">
-  <link href="css/styles.css" rel="stylesheet" type="text/css">
+  <link href="css/accomodation.css" rel="stylesheet" type="text/css">
   <title>Accomodations | The Grand Sweden Hotel</title>
 </head>
-
 <body>
+<section class = "rooms sec-width" id = "rooms">
+         <div class = "title">
+             <h2>rooms</h2>
+         </div>
+            <div class = "services-container">
+           <div class = "rooms-container"> 
+             <article class = "room">
+                 <div class = "room-image">
+                     <img src = "assets/image/s1.jpg" alt = "room image">
+                 </div>
+                 <div class = "room-text">
+                     <h3>Suite Room</h3>
+                     <ul>
+                         <li>
+                             <i class = "fas fa-arrow-alt-circle-right"></i>
+                             Lorem ipsum dolor sit amet.
+                         </li>
+                         <li>
+                             <i class = "fas fa-arrow-alt-circle-right"></i>
+                             Lorem ipsum dolor sit amet.
+                         </li>
+                         <li>
+                             <i class = "fas fa-arrow-alt-circle-right"></i>
+                             Lorem ipsum dolor sit amet.
+                         </li>
+                     </ul>
+                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus exercitationem repellendus maxime ullam tempore architecto provident unde expedita quam beatae, dolore eligendi molestias sint tenetur incidunt voluptas. Unde corporis qui iusto vitae. Aut nesciunt id iste, cum esse commodi nemo?</p>
+                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla corporis quasi officiis cumque, fugiat nostrum sunt, tempora animi dicta laborum beatae ratione doloremque. Delectus odio sit eius labore, atque quo?</p>     
+                     <button type = "button" class = "btn">book now</button>
+                 </div>
+             </article>
+             <article class = "room">
+                 <div class = "room-image">
+                     <img src = "assets/image/s2.jpg" alt = "room image">
+                 </div>
+                 <div class = "room-text">
+                     <h3>Suite Room</h3>
+                     <ul>
+                         <li>
+                             <i class = "fas fa-arrow-alt-circle-right"></i>
+                             Lorem ipsum dolor sit amet.
+                         </li>
+                         <li>
+                             <i class = "fas fa-arrow-alt-circle-right"></i>
+                             Lorem ipsum dolor sit amet.
+                         </li>
+                         <li>
+                             <i class = "fas fa-arrow-alt-circle-right"></i>
+                             Lorem ipsum dolor sit amet.
+                         </li>
+                     </ul>
+                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus exercitationem repellendus maxime ullam tempore architecto provident unde expedita quam beatae, dolore eligendi molestias sint tenetur incidunt voluptas. Unde corporis qui iusto vitae. Aut nesciunt id iste, cum esse commodi nemo?</p>
+                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla corporis quasi officiis cumque, fugiat nostrum sunt, tempora animi dicta laborum beatae ratione doloremque. Delectus odio sit eius labore, atque quo?</p>
+                     
+                     <button type = "button" class = "btn">book now</button>
+                 </div>
+             </article>
+             <article class = "room">
+                 <div class = "room-image">
+                     <img src = "assets/image/1.jpg" alt = "room image">
+                 </div>
+                 <div class = "room-text">
+                     <h3>Luxury Rooms</h3>
+                     <ul>
+                         <li>
+                             <i class = "fas fa-arrow-alt-circle-right"></i>
+                             Lorem ipsum dolor sit amet.
+                         </li>
+                         <li>
+                             <i class = "fas fa-arrow-alt-circle-right"></i>
+                             Lorem ipsum dolor sit amet.
+                         </li>
+                         <li>
+                             <i class = "fas fa-arrow-alt-circle-right"></i>
+                             Lorem ipsum dolor sit amet.
+                         </li>
+                     </ul>
+                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus exercitationem repellendus maxime ullam tempore architecto provident unde expedita quam beatae, dolore eligendi molestias sint tenetur incidunt voluptas. Unde corporis qui iusto vitae. Aut nesciunt id iste, cum esse commodi nemo?</p>
+                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla corporis quasi officiis cumque, fugiat nostrum sunt, tempora animi dicta laborum beatae ratione doloremque. Delectus odio sit eius labore, atque quo?</p>
+                     
+                     <button type = "button" class = "btn">book now</button>
+                 </div>
+             </article>
+       <article class = "room">
+                 <div class = "room-image">
+                     <img src = "assets/image/2.jpg" alt = "room image">
+                 </div>
+                 <div class = "room-text">
+                     <h3>Luxury Rooms</h3>
+                     <ul>
+                         <li>
+                             <i class = "fas fa-arrow-alt-circle-right"></i>
+                             Lorem ipsum dolor sit amet.
+                         </li>
+                         <li>
+                             <i class = "fas fa-arrow-alt-circle-right"></i>
+                             Lorem ipsum dolor sit amet.
+                         </li>
+                         <li>
+                             <i class = "fas fa-arrow-alt-circle-right"></i>
+                             Lorem ipsum dolor sit amet.
+                         </li>
+                     </ul>
+                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus exercitationem repellendus maxime ullam tempore architecto provident unde expedita quam beatae, dolore eligendi molestias sint tenetur incidunt voluptas. Unde corporis qui iusto vitae. Aut nesciunt id iste, cum esse commodi nemo?</p>
+                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla corporis quasi officiis cumque, fugiat nostrum sunt, tempora animi dicta laborum beatae ratione doloremque. Delectus odio sit eius labore, atque quo?</p>
+                     
+                     <button type = "button" class = "btn">book now</button>
+                 </div>
+             </article>
+       <article class = "room">
+                 <div class = "room-image">
+                     <img src = "assets/image/3.jpg" alt = "room image">
+                 </div>
+                 <div class = "room-text">
+                     <h3>Luxury Rooms</h3>
+                     <ul>
+                         <li>
+                             <i class = "fas fa-arrow-alt-circle-right"></i>
+                             Lorem ipsum dolor sit amet.
+                         </li>
+                         <li>
+                             <i class = "fas fa-arrow-alt-circle-right"></i>
+                             Lorem ipsum dolor sit amet.
+                         </li>
+                         <li>
+                             <i class = "fas fa-arrow-alt-circle-right"></i>
+                             Lorem ipsum dolor sit amet.
+                         </li>
+                     </ul>
+                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus exercitationem repellendus maxime ullam tempore architecto provident unde expedita quam beatae, dolore eligendi molestias sint tenetur incidunt voluptas. Unde corporis qui iusto vitae. Aut nesciunt id iste, cum esse commodi nemo?</p>
+                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla corporis quasi officiis cumque, fugiat nostrum sunt, tempora animi dicta laborum beatae ratione doloremque. Delectus odio sit eius labore, atque quo?</p>
+                    
+                     <button type = "button" class = "btn">book now</button>
+                 </div>
+             </article>
+       <article class = "room">
+                 <div class = "room-image">
+                     <img src = "assets/image/4.jpg" alt = "room image">
+                 </div>
+                 <div class = "room-text">
+                     <h3>Luxury Rooms</h3>
+                     <ul>
+                         <li>
+                             <i class = "fas fa-arrow-alt-circle-right"></i>
+                             Lorem ipsum dolor sit amet.
+                         </li>
+                         <li>
+                             <i class = "fas fa-arrow-alt-circle-right"></i>
+                             Lorem ipsum dolor sit amet.
+                         </li>
+                         <li>
+                             <i class = "fas fa-arrow-alt-circle-right"></i>
+                             Lorem ipsum dolor sit amet.
+                         </li>
+                     </ul>
+                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus exercitationem repellendus maxime ullam tempore architecto provident unde expedita quam beatae, dolore eligendi molestias sint tenetur incidunt voluptas. Unde corporis qui iusto vitae. Aut nesciunt id iste, cum esse commodi nemo?</p>
+                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla corporis quasi officiis cumque, fugiat nostrum sunt, tempora animi dicta laborum beatae ratione doloremque. Delectus odio sit eius labore, atque quo?</p>
+                    
+                     <button type = "button" class = "btn">book now</button>
+                 </div>
+             </article>
+       <article class = "room">
+                 <div class = "room-image">
+                     <img src = "assets/image/5.jpg" alt = "room image">
+                 </div>
+                 <div class = "room-text">
+                     <h3>Luxury Rooms</h3>
+                     <ul>
+                         <li>
+                             <i class = "fas fa-arrow-alt-circle-right"></i>
+                             Lorem ipsum dolor sit amet.
+                         </li>
+                         <li>
+                             <i class = "fas fa-arrow-alt-circle-right"></i>
+                             Lorem ipsum dolor sit amet.
+                         </li>
+                         <li>
+                             <i class = "fas fa-arrow-alt-circle-right"></i>
+                             Lorem ipsum dolor sit amet.
+                         </li>
+                     </ul>
+                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus exercitationem repellendus maxime ullam tempore architecto provident unde expedita quam beatae, dolore eligendi molestias sint tenetur incidunt voluptas. Unde corporis qui iusto vitae. Aut nesciunt id iste, cum esse commodi nemo?</p>
+                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla corporis quasi officiis cumque, fugiat nostrum sunt, tempora animi dicta laborum beatae ratione doloremque. Delectus odio sit eius labore, atque quo?</p>
+                     
+                     <button type = "button" class = "btn">book now</button>
+                 </div>
+             </article>
+       <article class = "room">
+                 <div class = "room-image">
+                     <img src = "assets/image/6.jpg" alt = "room image">
+                 </div>
+                 <div class = "room-text">
+                     <h3>Luxury Rooms</h3>
+                     <ul>
+                         <li>
+                             <i class = "fas fa-arrow-alt-circle-right"></i>
+                             Lorem ipsum dolor sit amet.
+                         </li>
+                         <li>
+                             <i class = "fas fa-arrow-alt-circle-right"></i>
+                             Lorem ipsum dolor sit amet.
+                         </li>
+                         <li>
+                             <i class = "fas fa-arrow-alt-circle-right"></i>
+                             Lorem ipsum dolor sit amet.
+                         </li>
+                     </ul>
+                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus exercitationem repellendus maxime ullam tempore architecto provident unde expedita quam beatae, dolore eligendi molestias sint tenetur incidunt voluptas. Unde corporis qui iusto vitae. Aut nesciunt id iste, cum esse commodi nemo?</p>
+                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla corporis quasi officiis cumque, fugiat nostrum sunt, tempora animi dicta laborum beatae ratione doloremque. Delectus odio sit eius labore, atque quo?</p>
+                     
+                     <button type = "button" class = "btn">book now</button>
+                 </div>
+             </article>
+         </div>
+     </section>
 </body>
-
 </html>
--- AdminBookings.php
+++ AdminBookings.php
@@ -7,11 +7,39 @@
 
 <head>
   <link href="" rel="icon" sizes="16x16">
-  <link href="css/styles.css" rel="stylesheet" type="text/css">
+  <link href="css/admin1.css" rel="stylesheet" type="text/css">
   <title>AdminBookings | The Grand Sweden Hotel</title>
 </head>
-
 <body>
-</body>
+    <div class="table-container">
+      <h2 class="table-heading">Hotel Reservations</h2>
 
-</html>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Check-in</th>
+            <th>Check-out</th>
+			 <th>Adult</th>
+            <th>Children</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+
+        <tbody>
+         <tr>
+              <!-- SAMPLE -->
+			  <tr>
+            <td data-heading="Name">Dea</td>
+            <td data-heading="Check-in">xxx</td>
+            <td data-heading="Check-out">xx</td>
+			<td data-heading="Adult">1</td>
+            <td data-heading="Children">0</td>
+            <td data-heading="Price">xxxx</td>
+          </tr>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </body>
+	  </html>
