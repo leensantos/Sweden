54e78e3d11cb2bb15cb7734d2b9bf8ba1451c14d
KrystelD <123krystel123@gmail.com>
2021-02-09 19:40:55 +0800

    update

--- AdminBookings.php
+++ AdminBookings.php
@@ -9,37 +9,49 @@
   <link href="" rel="icon" sizes="16x16">
   <link href="css/admin1.css" rel="stylesheet" type="text/css">
   <title>AdminBookings | The Grand Sweden Hotel</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 <body>
-    <div class="table-container">
-      <h2 class="table-heading">Hotel Reservations</h2>
-
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Check-in</th>
-            <th>Check-out</th>
-			 <th>Adult</th>
-            <th>Children</th>
-            <th>Price</th>
-          </tr>
-        </thead>
-
-        <tbody>
-         <tr>
-              <!-- SAMPLE -->
-			  <tr>
-            <td data-heading="Name">Dea</td>
-            <td data-heading="Check-in">xxx</td>
-            <td data-heading="Check-out">xx</td>
-			<td data-heading="Adult">1</td>
-            <td data-heading="Children">0</td>
-            <td data-heading="Price">xxxx</td>
-          </tr>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  </body>
-	  </html>
+  
+<div class="container">
+<h1>Hotel Reservations</h1>
+  <table class="rwd-table">
+    <tbody>
+      <tr>
+        <th>Name</th>
+        <th>Check-in</th>
+		 <th>Check-out</th>
+		  <th>Adult</th>
+		   <th>Children</th>
+		    <th>Price</th>
+      </tr>
+	  
+	  <!-- SAMPLE RESULT --->
+      <tr>
+        <td data-th="Name">
+		Dea
+        </td>
+		<td data-th="Check-in">
+		xxx
+        </td>
+		<td data-th="Check-out">
+		xxx
+        </td>
+		<td data-th="Adult">
+		xxx
+        </td>
+		<td data-th="Children">
+		xxx
+        </td>
+		<td data-th="Price">
+		xxx
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+</body>
+</html>
--- AdminPage.php
+++ AdminPage.php
@@ -3,15 +3,126 @@
 ?>
 
 <!DOCTYPE html>
-<html>
-
-<head>
-  <link href="" rel="icon" sizes="16x16">
-  <link href="css/styles.css" rel="stylesheet" type="text/css">
-  <title>AdminPage | The Grand Sweden Hotel</title>
-</head>
-
-<body>
+<html lang="en">
+  <head>
+    <link href="" rel="icon" sizes="16x16">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <title>AdminPage | The Grand Sweden Hotel</title>
+    <link rel="stylesheet" href="css/admin2.css" type="text/css"/>
+  </head>
+  <body>
+  
+<div class="container">
+<h1>Admin Dashboard</h1>
+  <table class="rwd-table">
+    <tbody>
+      <tr>
+        <th></th>
+        <th>Edit</th>
+        <th>View</th>
+      </tr>
+      <tr>
+        <td data-th="Supplier Code">
+		Booking
+        </td>
+        <td data-th="Edit">  <a href="#">
+          <span class="glyphicon glyphicon-edit"></span>
+        </a>
+        </td>
+        <td data-th="View">
+		 <a href="AdminBooking.php">
+           <span class="glyphicon glyphicon-eye-open"></span>
+        </a>
+        </td>
+      </tr>
+      <tr>
+        <td data-th="Supplier Code">
+		Accomodation
+        </td>
+        <td data-th="Edit">  <a href="#">
+          <span class="glyphicon glyphicon-edit"></span>
+        </a>
+        </td>
+        <td data-th="View">
+		 <a href="Accomodations.php">
+           <span class="glyphicon glyphicon-eye-open"></span>
+        </a>
+        </td>
+      </tr>
+	  <tr>
+        <td data-th="Supplier Code">
+		Gallery
+        </td>
+        <td data-th="Edit">  <a href="#">
+          <span class="glyphicon glyphicon-edit"></span>
+        </a>
+        </td>
+        <td data-th="View">
+		 <a href="Gallery.php">
+           <span class="glyphicon glyphicon-eye-open"></span>
+        </a>
+        </td>
+      </tr>
+	  <tr>
+        <td data-th="Supplier Code">
+		Destination
+        </td>
+        <td data-th="Edit">  <a href="#">
+          <span class="glyphicon glyphicon-edit"></span>
+        </a>
+        </td>
+        <td data-th="View">
+		 <a href="Destination.php">
+           <span class="glyphicon glyphicon-eye-open"></span>
+        </a>
+        </td>
+      </tr>
+	  <tr>
+        <td data-th="Supplier Code">
+		Amenities
+        </td>
+        <td data-th="Edit">  <a href="#">
+          <span class="glyphicon glyphicon-edit"></span>
+        </a>
+        </td>
+        <td data-th="View">
+		 <a href="Amenities.php">
+           <span class="glyphicon glyphicon-eye-open"></span>
+        </a>
+        </td>
+      </tr>
+	  <tr>
+        <td data-th="Supplier Code">
+		Experiences
+        </td>
+        <td data-th="Edit">  <a href="#">
+          <span class="glyphicon glyphicon-edit"></span>
+        </a>
+        </td>
+        <td data-th="View">
+		 <a href="Experience.php">
+           <span class="glyphicon glyphicon-eye-open"></span>
+        </a>
+        </td>
+      </tr>
+	  <tr>
+        <td data-th="Supplier Code">
+		Gallery
+        </td>
+        <td data-th="Edit">  <a href="#">
+          <span class="glyphicon glyphicon-edit"></span>
+        </a>
+        </td>
+        <td data-th="View">
+		 <a href="Gallery.php">
+           <span class="glyphicon glyphicon-eye-open"></span>
+        </a>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</div>
 </body>
-
 </html>
