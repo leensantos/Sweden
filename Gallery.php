0b134fe4de6831168279647af50bea9ae3eb5c01
cjariola <ariola.cydel@gmail.com>
2021-02-10 01:23:31 +0800

    hehe

--- Gallery.php
+++ Gallery.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/gallery.css">
+    <link rel="stylesheet" href="css/footer2.css">
     <title>Document</title>
 </head>
 <body>
