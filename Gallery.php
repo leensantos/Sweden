916b0154627b7e7d65cac34f1b64bc657e74d36a
cjariola <ariola.cydel@gmail.com>
2021-02-11 00:31:35 +0800

    dwmaidmwaoidaw

--- Gallery.php
+++ Gallery.php
@@ -1,100 +1,105 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+    <link rel="stylesheet" type="text/css"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/footer2.css">
     <link rel="stylesheet" href="css/navi.css">
     <link rel="stylesheet" href="css/gal.css">
     <title>Document</title>
 </head>
+
 <body>
-<nav>
-	<input id="nav-toggle" type="checkbox">
-	<div class="logo">GRAND SWEDEN HOTEL</div>
-	<ul class="links">
-		<li><a href="index.php">Home</a></li>
-		<li><a href="#about">Accomodation</a></li>
-		<li><a href="#Experiences">Experiences</a></li>
-		<li><a href="#Amenities">Amenities</a></li>
-		<li><a href="#Offers">Offers</a></li>
-		<li><a href="#Gallery">Gallery</a></li>
-		<li><a href="#Destination">Destination</a></li>
-		<li><a href="#Contact Us">Contact</a></li>
-		<li><a href="#Profile">Profile</a></li>
-	</ul>
-	<label for="nav-toggle" class="icon-burger">
-		<div class="line"></div>
-		<div class="line"></div>
-		<div class="line"></div>
-	</label>
-</nav>
-<br><br><br><br><br><br>
-<div class="container">
-		<h1>GALLERY</h1>
-		<h2> Because every picture tells a story, let me help you tell yours </h2><BR>
-		<hr></div>
-
-		
-		<div class="main">
-			<img src="assets/gallery/pool.jpg" alt="">
-			<img src="assets/gallery/bar.jpg" alt="">
-			<img src="assets/gallery/restau.jpg" alt="">
-			<img src="assets/gallery/gym.jpg" alt="">
-			<img src="assets/gallery/pool.jpg" alt="">
-			<img src="assets/gallery/bar.jpg" alt="">
-			<img src="assets/gallery/restau.jpg" alt="">
-			<img src="assets/gallery/gym.jpg" alt="">
-			<img src="assets/gallery/pool.jpg" alt="">
-			<img src="assets/gallery/bar.jpg" alt="">
-			<img src="assets/gallery/restau.jpg" alt="">
-			<img src="assets/gallery/gym.jpg" alt="">
-		</div>
-
-		<br><br><br>
-<footer style="background-color:black;">
- <br><br><br>
+    <nav>
+        <input id="nav-toggle" type="checkbox">
+        <div class="logo">GRAND SWEDEN HOTEL</div>
+        <ul class="links">
+            <li><a href="index.php">Home</a></li>
+            <li><a href="#about">Accomodation</a></li>
+            <li><a href="#Experiences">Experiences</a></li>
+            <li><a href="#Amenities">Amenities</a></li>
+            <li><a href="#Offers">Offers</a></li>
+            <li><a href="#Gallery">Gallery</a></li>
+            <li><a href="#Destination">Destination</a></li>
+            <li><a href="#Contact Us">Contact</a></li>
+            <li><a href="#Profile">Profile</a></li>
+        </ul>
+        <label for="nav-toggle" class="icon-burger">
+            <div class="line"></div>
+            <div class="line"></div>
+            <div class="line"></div>
+        </label>
+    </nav>
+    <br><br><br><br><br><br>
     <div class="container">
-  	 	<div class="row">
-  	 		<div class="footer-col">
-  	 			<h4>Grand Sweden Hotel</h4>
-  	 			<ul>
-  	 				<li><a href="#">Home</a></li>
-  	 				<li><a href="#">Accomodation</a></li>
-  	 				<li><a href="#">Experiences</a></li>
-  	 				<li><a href="#">Amenities</a></li>
-                    <li><a href="#">Offers</a></li>
-  	 				<li><a href="#">Gallery</a></li>
-  	 				<li><a href="#">Destination</a></li>
-  	 				<li><a href="#">Contact Us</a></li>
-  	 			</ul>
-  	 		</div>
-  	 		<div class="footer-col">
-  	 			<h4>Get in touch</h4>
-  	 			<ul>
-  	 				<li><a href="#">PO Box 222 333 Sen. Gil J. Puyat Ave, Makati</a></li>
-  	 				<li><a href="#">+632 8247-5000</a></li>
-  	 				<li><a href="#">Privacy Policy</a></li>
-  	 			</ul>
-  	 		</div>
-  	 		<div class="footer-col">
-  	 			<h4>follow us</h4>
-  	 			<div class="social-links">
-  	 				<a href="#"><i class="fab fa-facebook-f"></i></a>
-  	 				<a href="#"><i class="fab fa-twitter"></i></a>
-  	 				<a href="#"><i class="fab fa-instagram"></i></a>
-  	 				<a href="#"><i class="fab fa-youtube"></i></a>
-  	 			</div>
-  	 		</div>
-  	 	</div>
-           <center>
-           <br>
-           <a style="color: #bbbbbb;">© Copyright 2021. All Rights Reserved.</a>
+        <h1>Gallery</h1>
+        <h2> Because every picture tells a story, let me help you tell yours </h2><BR>
+        <hr>
+    </div>
+
+
+    <div class="main">
+        <img src="assets/gallery/pool.jpg" alt="">
+        <img src="assets/gallery/bar.jpg" alt="">
+        <img src="assets/gallery/restau.jpg" alt="">
+        <img src="assets/gallery/gym.jpg" alt="">
+        <img src="assets/gallery/pool.jpg" alt="">
+        <img src="assets/gallery/bar.jpg" alt="">
+        <img src="assets/gallery/restau.jpg" alt="">
+        <img src="assets/gallery/gym.jpg" alt="">
+        <img src="assets/gallery/pool.jpg" alt="">
+        <img src="assets/gallery/bar.jpg" alt="">
+        <img src="assets/gallery/restau.jpg" alt="">
+        <img src="assets/gallery/gym.jpg" alt="">
+    </div>
+
+    <br><br><br>
+    <footer style="background-color: #413b3b ;">
+        <br><br><br>
+        <div class="container">
+            <div class="row">
+                <div class="footer-col">
+                    <h4>Grand Sweden Hotel</h4>
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Accomodation</a></li>
+                        <li><a href="#">Experiences</a></li>
+                        <li><a href="#">Amenities</a></li>
+                        <li><a href="#">Offers</a></li>
+                        <li><a href="#">Gallery</a></li>
+                        <li><a href="#">Destination</a></li>
+                        <li><a href="#">Contact Us</a></li>
+                    </ul>
+                </div>
+                <div class="footer-col">
+                    <h4>Get in touch</h4>
+                    <ul>
+                        <li><a href="#">PO Box 222 333 Sen. Gil J. Puyat Ave, Makati</a></li>
+                        <li><a href="#">+632 8247-5000</a></li>
+                        <li><a href="#">Privacy Policy</a></li>
+                    </ul>
+                </div>
+                <div class="footer-col">
+                    <h4>follow us</h4>
+                    <div class="social-links">
+                        <a href="#"><i class="fab fa-facebook-f"></i></a>
+                        <a href="#"><i class="fab fa-twitter"></i></a>
+                        <a href="#"><i class="fab fa-instagram"></i></a>
+                        <a href="#"><i class="fab fa-youtube"></i></a>
+                    </div>
+                </div>
+            </div>
+            <center>
+                <br>
+                <a style="color: #bbbbbb;">© Copyright 2021. All Rights Reserved.</a>
             </center>
-       </div>
-       <br>   
-    </footer> 
+        </div>
+        <br>
+    </footer>
 </body>
+
 </html>
