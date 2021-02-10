9373f9c3ee19f0ad16688d2dba328473b1677a80
leensantos <me4299@gmail.com>
2021-02-11 05:18:07 +0800

    hommm

--- js/sign-in-script.js
+++ js/sign-in-script.js
@@ -4,21 +4,21 @@ const loginBtn = document.querySelectorAll(".login-btn"),
     box = document.querySelector(".box"),
     loginForm = document.querySelector(".login-form"),
     registerForm = document.querySelector(".register-form"),
-    lostPasswordForm = document.querySelector(".lost-password-form"),
-    // boxFormSignUp = document.querySelector(".box-form-sign-up"),
-    // boxFormSignIn = document.querySelector(".box-form-sign-in");
+    lostPasswordForm = document.querySelector(".lost-password-form");
+// boxFormSignUp = document.querySelector(".box-form-sign-up"),
+// boxFormSignIn = document.querySelector(".box-form-sign-in");
 
-    registerBtn.forEach((btn) => {
-        btn.addEventListener("click", () => {
-            box.classList.add("slide-active");
-            registerForm.classList.remove("form-hidden");
-            loginForm.classList.add("form-hidden");
-            lostPasswordForm.classList.add("form-hidden");
-            // boxFormSignUp.classList.add("box-hidden");
-            // boxFormSignIn.classList.remove("box-hidden");
-            // boxFormSignIn.classList.add("box-view");
-        });
+registerBtn.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        box.classList.add("slide-active");
+        registerForm.classList.remove("form-hidden");
+        loginForm.classList.add("form-hidden");
+        lostPasswordForm.classList.add("form-hidden");
+        // boxFormSignUp.classList.add("box-hidden");
+        // boxFormSignIn.classList.remove("box-hidden");
+        // boxFormSignIn.classList.add("box-view");
     });
+});
 
 loginBtn.forEach((btn) => {
     btn.addEventListener("click", () => {
