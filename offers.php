0855abcc821e5e8c03623322bcb8054f5d2f8539
cjariola <ariola.cydel@gmail.com>
2021-02-11 14:39:19 +0800

    offers

--- offers.php
+++ offers.php
@@ -5,171 +5,177 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+    <link rel="stylesheet" href="css/navi.css">
+    <link rel="stylesheet" href="css/foot.css">
     <link rel="stylesheet" href="css/experiences2.css">
     <script src="https://kit.fontawesome.com/a076d05399.js"></script>
 </head>
 
 <body>
-    <div class="wrapper">
-        <div class="center-line">
-            <a href="#" class="scroll-icon"><i class="fas fa-caret-up"></i></a>
-        </div>
-        <div class="row row-1">
-            <section>
-                <i class="icon fas fa-home"></i>
-                <div class="details">
-                    <span class="title">Title of Section 1</span>
-                    <span>3rd Jan 2021</span>
-                </div>
-                <p>Lorem ipsum dolor sit ameters consectetur adipisicing elit. Sed qui veroes praesentium maiores, sint
-                    eos vero sapiente voluptas debitis dicta dolore.</p>
-                <div class="bottom">
-                    <a href="#">Read more</a>
-                </div>
-            </section>
-        </div>
-        <div class="row row-2">
-            <section>
-                <i class="icon fas fa-star"></i>
-                <div class="details">
-                    <span class="title">Title of Section 2</span>
-                    <span>3rd Jan 2021</span>
-                </div>
-                <p>Lorem ipsum dolor sit ameters consectetur adipisicing elit. Sed qui veroes praesentium maiores, sint
-                    eos vero sapiente voluptas debitis dicta dolore.</p>
-                <div class="bottom">
-                    <a href="#">Read more</a>
-                </div>
-            </section>
-        </div>
-        <div class="row row-1">
-            <section>
-                <i class="icon fas fa-rocket"></i>
-                <div class="details">
-                    <span class="title">Title of Section 3</span>
-                    <span>3rd Jan 2021</span>
-                </div>
-                <p>Lorem ipsum dolor sit ameters consectetur adipisicing elit. Sed qui veroes praesentium maiores, sint
-                    eos vero sapiente voluptas debitis dicta dolore.</p>
-                <div class="bottom">
-                    <a href="#">Read more</a>
-                    <i>- Someone famous</i>
-                </div>
-            </section>
-        </div>
-        <div class="row row-2">
-            <section>
-                <i class="icon fas fa-globe"></i>
-                <div class="details">
-                    <span class="title">Title of Section 4</span>
-                    <span>4th Jan 2021</span>
-                </div>
-                <p>Lorem ipsum dolor sit ameters consectetur adipisicing elit. Sed qui veroes praesentium maiores, sint
-                    eos vero sapiente voluptas debitis dicta dolore.</p>
-                <div class="bottom">
-                    <a href="#">Read more</a>
-                    <i>- Someone famous</i>
-                </div>
-            </section>
-        </div>
-        <div class="row row-1">
-            <section>
-                <i class="icon fas fa-paper-plane"></i>
-                <div class="details">
-                    <span class="title">Title of Section 5</span>
-                    <span>5th Jan 2021</span>
-                </div>
-                <p>Lorem ipsum dolor sit ameters consectetur adipisicing elit. Sed qui veroes praesentium maiores, sint
-                    eos vero sapiente voluptas debitis dicta dolore.</p>
-                <div class="bottom">
-                    <a href="#">Read more</a>
-                    <i>- Someone famous</i>
-                </div>
-            </section>
-        </div>
-        <div class="row row-2">
-            <section>
-                <i class="icon fas fa-map-marker-alt"></i>
-                <div class="details">
-                    <span class="title">Title of Section 6</span>
-                    <span>6th Jan 2021</span>
-                </div>
-                <p>Lorem ipsum dolor sit ameters consectetur adipisicing elit. Sed qui veroes praesentium maiores, sint
-                    eos vero sapiente voluptas debitis dicta dolore.</p>
-                <div class="bottom">
-                    <a href="#">Read more</a>
-                    <i>- Someone famous</i>
-                </div>
-            </section>
-            <!-- <section>
+    <nav>
+        <input id="nav-toggle" type="checkbox">
+        <div class="logo">GRAND SWEDEN HOTEL</div>
+        <ul class="links">
+            <li><a href="index.php">Home</a></li>
+            <li><a href="#about">Accomodation</a></li>
+            <li><a href="#Experiences">Experiences</a></li>
+            <li><a href="#Offers">Offers</a></li>
+            <li><a href="#Gallery">Gallery</a></li>
+            <li><a href="#Destination">Destination</a></li>
+            <li><a href="#Contact Us">Contact</a></li>
+            <li><a href="#Profile">Profile</a></li>
+        </ul>
+        <label for="nav-toggle" class="icon-burger">
+            <div class="line"></div>
+            <div class="line"></div>
+            <div class="line"></div>
+        </label>
+    </nav><br><br><br><br><br><br>
+
+    <section>
         <div class="wrapper">
-            <div class="box">
-                <i class="fas fa-quote-left quote"></i>
-                <p>Lorem aliasry ipsum dolor sits ametans, consectetur adipisicing elitits. Expedita reiciendis itaque
-                    placeat thuratu, quasi yiuos repellendus repudiandae deleniti ideas fuga molestiae, alias.</p>
-                <div class="content">
-                    <div class="info">
-                        <div class="name">Swimming Pool</div>
-                        <div class="job">Designer | Developer</div>
-                        <div class="stars">
-                            <i class="fas fa-star"></i>
-                            <i class="fas fa-star"></i>
-                            <i class="far fa-star"></i>
-                            <i class="far fa-star"></i>
-                            <i class="far fa-star"></i>
-                        </div>
-                    </div>
-                    <div class="image">
-                        <img src="assets/2.jpg" alt="">
-                    </div>
-                </div><BR><BR>
-                <center> AWIT </center>
+            <div class="center-line">
+                <a href="#" class="scroll-icon"><i class="fas fa-caret-up"></i></a>
             </div>
-            <div class="box">
-                <i class="fas fa-quote-left quote"></i>
-                <p>Lorem aliasry ipsum dolor sits ametans, consectetur adipisicing elitits. Expedita reiciendis itaque
-                    placeat thuratu, quasi yiuos repellendus repudiandae deleniti ideas fuga molestiae, alias.</p>
-                <div class="content">
-                    <div class="info">
-                        <div class="name">Spa and Billard</div>
-                        <div class="job">YouTuber | Blogger</div>
-                        <div class="stars">
-                            <i class="fas fa-star"></i>
-                            <i class="fas fa-star"></i>
-                            <i class="fas fa-star"></i>
-                            <i class="far fa-star"></i>
-                            <i class="far fa-star"></i>
-                        </div>
+            <div class="row row-1">
+                <section>
+                    <i class="icon fas fa-home"></i>
+                    <div class="details">
+                        <span class="title">New Year Promo</span>
+                        <span>3rd Jan 2021</span>
                     </div>
-                    <div class="image">
-                        <img src="assets/2.jpg" alt="">
+                    <p>Lorem ipsum dolor sit ameters consectetur adipisicing elit. Sed qui veroes praesentium maiores,
+                        sint
+                        eos vero sapiente voluptas debitis dicta dolore.</p>
+                    <div class="bottom">
+                        <a href="#">Read more</a>
                     </div>
-                </div><BR><BR>
-                <center> AWIT </center>
+                </section>
             </div>
-            <div class="box">
-                <i class="fas fa-quote-left  quote"></i>
-                <p>Lorem aliasry ipsum dolor sits ametans, consectetur adipisicing elitits. Expedita reiciendis itaque
-                    placeat thuratu, quasi yiuos repellendus repudiandae deleniti ideas fuga molestiae, alias.</p>
-                <div class="content">
-                    <div class="info">
-                        <div class="name">Kristina Bellis</div>
-                        <div class="job">Freelancer | Advertiser</div>
-                        <div class="stars">
-                            <i class="fas fa-star"></i>
-                            <i class="fas fa-star"></i>
-                            <i class="fas fa-star"></i>
-                            <i class="fas fa-star"></i>
-                            <i class="far fa-star"></i>
-                        </div>
+            <div class="row row-2">
+                <section>
+                    <i class="icon fas fa-star"></i>
+                    <div class="details">
+                        <span class="title">New Year Promo</span>
+                        <span>3rd Jan 2021</span>
                     </div>
-                    <div class="image">
-                        <img src="assets/2.jpg" alt="">
+                    <p>Lorem ipsum dolor sit ameters consectetur adipisicing elit. Sed qui veroes praesentium maiores,
+                        sint
+                        eos vero sapiente voluptas debitis dicta dolore.</p>
+                    <div class="bottom">
+                        <a href="#">Read more</a>
                     </div>
-                </div><BR><BR>
-                <center> AWIT </center>
+                </section>
             </div>
-        </div> -->
+            <div class="row row-1">
+                <section>
+                    <i class="icon fas fa-heart"></i>
+                    <div class="details">
+                        <span class="title">New Year Promo</span>
+                        <span>3rd Jan 2021</span>
+                    </div>
+                    <p>Lorem ipsum dolor sit ameters consectetur adipisicing elit. Sed qui veroes praesentium maiores,
+                        sint
+                        eos vero sapiente voluptas debitis dicta dolore.</p>
+                    <div class="bottom">
+                        <a href="#">Read more</a>
+                        <i>- Someone famous</i>
+                    </div>
+                </section>
+            </div>
+            <div class="row row-2">
+                <section>
+                    <i class="icon fas fa-globe"></i>
+                    <div class="details">
+                        <span class="title">New Year Promo</span>
+                        <span>4th Jan 2021</span>
+                    </div>
+                    <p>Lorem ipsum dolor sit ameters consectetur adipisicing elit. Sed qui veroes praesentium maiores,
+                        sint
+                        eos vero sapiente voluptas debitis dicta dolore.</p>
+                    <div class="bottom">
+                        <a href="#">Read more</a>
+                        <i>- Someone famous</i>
+                    </div>
+                </section>
+            </div>
+            <div class="row row-1">
+                <section>
+                    <i class="icon fas fa-paper-plane"></i>
+                    <div class="details">
+                        <span class="title">New Year Promo</span>
+                        <span>5th Jan 2021</span>
+                    </div>
+                    <p>Lorem ipsum dolor sit ameters consectetur adipisicing elit. Sed qui veroes praesentium maiores,
+                        sint
+                        eos vero sapiente voluptas debitis dicta dolore.</p>
+                    <div class="bottom">
+                        <a href="#">Read more</a>
+                        <i>- Someone famous</i>
+                    </div>
+                </section>
+            </div>
+            <div class="row row-2">
+                <section>
+                    <i class="icon fas fa-map-marker-alt"></i>
+                    <div class="details">
+                        <span class="title">New Year Promo</span>
+                        <span>6th Jan 2021</span>
+                    </div>
+                    <p>Lorem ipsum dolor sit ameters consectetur adipisicing elit. Sed qui veroes praesentium maiores,
+                        sint
+                        eos vero sapiente voluptas debitis dicta dolore.</p>
+                    <div class="bottom">
+                        <a href="#">Read more</a>
+                        <i>- Someone famous</i>
+                    </div>
+                </section>
+            </div>
+    </section><br><br><br><br>
+    <footer style="background-color: #413b3b ;">
+        <br><br><br>
+        <div class="container">
+            <div class="row">
+                <div class="footer-col">
+                    <h4>Grand Sweden Hotel</h4>
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Accomodation</a></li>
+                        <li><a href="#">Experiences</a></li>
+                        <li><a href="#">Offers</a></li>
+                        <li><a href="#">Gallery</a></li>
+                        <li><a href="#">Destination</a></li>
+                        <li><a href="#">Contact Us</a></li>
+                    </ul>
+                </div>
+                <div class="footer-col">
+                    <h4>Get in touch</h4>
+                    <ul>
+                        <li><a href="#">PO Box 222 333 Sen. Gil J. Puyat Ave, Makati</a></li>
+                        <li><a href="#">+632 8247-5000</a></li>
+                        <li><a href="#">Privacy Policy</a></li>
+                    </ul>
+                </div>
+                <div class="footer-col">
+                    <h4>follow us</h4>
+                    <div class="social-links">
+                        <a href="#"><i class="fab fa-facebook-f"></i></a>
+                        <a href="#"><i class="fab fa-twitter"></i></a>
+                        <a href="#"><i class="fab fa-instagram"></i></a>
+                        <a href="#"><i class="fab fa-youtube"></i></a>
+                    </div>
+                </div>
+            </div>
+            <center>
+                <br>
+                <a style="color: #bbbbbb;">© Copyright 2021. All Rights Reserved.</a>
+            </center>
+        </div>
+        <br>
+    </footer>
+
 </body>
 
 </html>
