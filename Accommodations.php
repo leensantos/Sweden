ee7937060c45897b67a96b8a275f53a3119cd631
KrystelD <123krystel123@gmail.com>
2021-02-11 19:19:12 +0800

    updateeee huhu

--- Accommodations.php
+++ Accommodations.php
@@ -6,167 +6,176 @@
 <html>
 
 <head>
-  <link href="" rel="icon" sizes="16x16">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link href="css/accomodation.css" rel="stylesheet" type="text/css">
-  <title>Accomodations | The Grand Sweden Hotel</title>
- 
+    <link href="" rel="icon" sizes="16x16">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="css/accomodation.css" rel="stylesheet" type="text/css">
+    <title>Accomodations | The Grand Sweden Hotel</title>
+
 </head>
+
 <body>
-   
 
-   <section class = "rooms sec-width" id = "rooms">
-       <div class = "title">
-           <h2>Rooms</h2>
-       </div>
-       <div class = "rooms-container">
-        
-           <article class = "room">
-               <div class = "room-image">
-                   <img src = "image/room1.jpg" alt = "room image">
-               </div>
-               <div class = "room-text"> <br>
-                   <h3>Luxury Rooms</h3>
-                   <ul>
-                       <li>
-                           <i class = "fas fa-arrow-alt-circle-right"></i>
-                           Lorem ipsum dolor sit amet.
-                       </li>
-                       <li>
-                           <i class = "fas fa-arrow-alt-circle-right"></i>
-                           Lorem ipsum dolor sit amet.
-                       </li>
-                       <li>
-                           <i class = "fas fa-arrow-alt-circle-right"></i>
-                           Lorem ipsum dolor sit amet.
-                       </li>
-                   </ul>
-                   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus exercitationem repellendus maxime ullam tempore architecto provident unde expedita quam beatae, dolore eligendi molestias sint tenetur incidunt voluptas. Unde corporis qui iusto vitae. Aut nesciunt id iste, cum esse commodi nemo?</p>
-                   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla corporis quasi officiis cumque, fugiat nostrum sunt, tempora animi dicta laborum beatae ratione doloremque. Delectus odio sit eius labore, atque quo?</p>
-                   <br>
-                   <button type = "button" class = "btn">book now</button>
-               </div>
-           </article>
-          
-        
-           <article class = "room">
-               <div class = "room-image">
-                   <img src = "image/room2.jpg" alt = "room image">
-               </div>
-               <div class = "room-text"><br>
-                   <h3>Luxury Rooms</h3>
-                   <ul>
-                       <li>
-                           <i class = "fas fa-arrow-alt-circle-right"></i>
-                           Lorem ipsum dolor sit amet.
-                       </li>
-                       <li>
-                           <i class = "fas fa-arrow-alt-circle-right"></i>
-                           Lorem ipsum dolor sit amet.
-                       </li>
-                       <li>
-                           <i class = "fas fa-arrow-alt-circle-right"></i>
-                           Lorem ipsum dolor sit amet.
-                       </li>
-                   </ul>
-                   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus exercitationem repellendus maxime ullam tempore architecto provident unde expedita quam beatae, dolore eligendi molestias sint tenetur incidunt voluptas. Unde corporis qui iusto vitae. Aut nesciunt id iste, cum esse commodi nemo?</p>
-                   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla corporis quasi officiis cumque, fugiat nostrum sunt, tempora animi dicta laborum beatae ratione doloremque. Delectus odio sit eius labore, atque quo?</p>
-                   <br>
-                   <button type = "button" class = "btn">book now</button>
-               </div>
-           </article>
-          
-        
-           <article class = "room">
-               <div class = "room-image">
-                   <img src = "image/room3.jpg" alt = "room image">
-               </div>
-               <div class = "room-text"><br>
-                   <h3>Luxury Rooms</h3>
-                   <ul>
-                       <li>
-                           <i class = "fas fa-arrow-alt-circle-right"></i>
-                           Lorem ipsum dolor sit amet.
-                       </li>
-                       <li>
-                           <i class = "fas fa-arrow-alt-circle-right"></i>
-                           Lorem ipsum dolor sit amet.
-                       </li>
-                       <li>
-                           <i class = "fas fa-arrow-alt-circle-right"></i>
-                           Lorem ipsum dolor sit amet.
-                       </li>
-                   </ul>
-                   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus exercitationem repellendus maxime ullam tempore architecto provident unde expedita quam beatae, dolore eligendi molestias sint tenetur incidunt voluptas. Unde corporis qui iusto vitae. Aut nesciunt id iste, cum esse commodi nemo?</p>
-                   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla corporis quasi officiis cumque, fugiat nostrum sunt, tempora animi dicta laborum beatae ratione doloremque. Delectus odio sit eius labore, atque quo?</p>
-                   <br>
-                   <button type = "button" class = "btn">book now</button>
-               </div>
-           </article>
-          
-       </div>
-   </section>
 
-  
-   <section class = "services sec-width" id = "services">
-       <div class = "title">
-           <h2>Services</h2>
-       </div>
-       <div class = "services-container">
-         
-           <article class = "service">
-               <div class = "service-icon">
-                   <span>
-                       <i class = "fas fa-utensils"></i>
-                   </span>
-               </div>
-               <div class = "service-content">
-                   <h2>Food Service/ Food Runner</h2>
-                   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias blanditiis tempore officia accusamus asperiores. Illum maxime eligendi necessitatibus laudantium iste nisi pariatur doloremque ut illo similique voluptatum enim distinctio perferendis, ad ipsam aspernatur omnis rem autem ex, reiciendis corporis suscipit!</p>
-                   <button type = "button" class = "btn">Know More</button>
-               </div>
-           </article>
-          
-         
-           <article class = "service">
-               <div class = "service-icon">
-                   <span>
-                       <i class = "fas fa-swimming-pool"></i>
-                   </span>
-               </div>
-               <div class = "service-content">
-                   <h2>Refreshment</h2>
-                   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias blanditiis tempore officia accusamus asperiores. Illum maxime eligendi necessitatibus laudantium iste nisi pariatur doloremque ut illo similique voluptatum enim distinctio perferendis, ad ipsam aspernatur omnis rem autem ex, reiciendis corporis suscipit!</p>
-                   <button type = "button" class = "btn">Know More</button>
-               </div>
-           </article>
-           <article class = "service">
-               <div class = "service-icon">
-                   <span>
-                       <i class = "fas fa-broom"></i>
-                   </span>
-               </div>
-               <div class = "service-content">
-                   <h2>Housekeeping</h2>
-                   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias blanditiis tempore officia accusamus asperiores. Illum maxime eligendi necessitatibus laudantium iste nisi pariatur doloremque ut illo similique voluptatum enim distinctio perferendis, ad ipsam aspernatur omnis rem autem ex, reiciendis corporis suscipit!</p>
-                   <button type = "button" class = "btn">Know More</button>
-               </div>
-           </article>
+    <section class="rooms sec-width" id="rooms">
+        <div class="title">
+            <h2>Rooms</h2>
+        </div>
+        <div class="rooms-container">
 
-           <article class = "service">
-               <div class = "service-icon">
-                   <span>
-                       <i class = "fas fa-door-closed"></i>
-                   </span>
-               </div>
-               <div class = "service-content">
-                   <h2>Room Security</h2>
-                   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias blanditiis tempore officia accusamus asperiores. Illum maxime eligendi necessitatibus laudantium iste nisi pariatur doloremque ut illo similique voluptatum enim distinctio perferendis, ad ipsam aspernatur omnis rem autem ex, reiciendis corporis suscipit!</p>
-                   <button type = "button" class = "btn">Know More</button>
-               </div>
-           </article>
-          
-       </div>
-   </section>
+            <article class="room">
+                <div class="room-image">
+                    <img src="assets/accomodation/room1.jpg" alt="room image">
+                </div>
+                <div class="room-text"> <br>
+                    <h3>Luxury Rooms</h3>
+                    <ul>
+                        <li>
+                            <i class="fas fa-arrow-alt-circle-right"></i>
+                            Lorem ipsum dolor sit amet.
+                        </li>
+                        <li>
+                            <i class="fas fa-arrow-alt-circle-right"></i>
+                            Lorem ipsum dolor sit amet.
+                        </li>
+                        <li>
+                            <i class="fas fa-arrow-alt-circle-right"></i>
+                            Lorem ipsum dolor sit amet.
+                        </li>
+                    </ul>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus exercitationem repellendus maxime ullam tempore architecto provident unde expedita quam beatae, dolore eligendi molestias sint tenetur incidunt voluptas. Unde corporis qui iusto vitae. Aut nesciunt id iste, cum esse commodi nemo?</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla corporis quasi officiis cumque, fugiat nostrum sunt, tempora animi dicta laborum beatae ratione doloremque. Delectus odio sit eius labore, atque quo?</p>
+                    <br>
+                    <a href="BookNow.php" style="text-decoration: none;">
+                        <button type="button" class="btn">book now</button>
+                    </a>
+                </div>
+            </article>
+
+
+            <article class="room">
+                <div class="room-image">
+                    <img src="assets/accomodation/room2.jpg" alt="room image">
+                </div>
+                <div class="room-text"><br>
+                    <h3>Luxury Rooms</h3>
+                    <ul>
+                        <li>
+                            <i class="fas fa-arrow-alt-circle-right"></i>
+                            Lorem ipsum dolor sit amet.
+                        </li>
+                        <li>
+                            <i class="fas fa-arrow-alt-circle-right"></i>
+                            Lorem ipsum dolor sit amet.
+                        </li>
+                        <li>
+                            <i class="fas fa-arrow-alt-circle-right"></i>
+                            Lorem ipsum dolor sit amet.
+                        </li>
+                    </ul>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus exercitationem repellendus maxime ullam tempore architecto provident unde expedita quam beatae, dolore eligendi molestias sint tenetur incidunt voluptas. Unde corporis qui iusto vitae. Aut nesciunt id iste, cum esse commodi nemo?</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla corporis quasi officiis cumque, fugiat nostrum sunt, tempora animi dicta laborum beatae ratione doloremque. Delectus odio sit eius labore, atque quo?</p>
+                    <br>
+                    <a href="BookNow.php" style="text-decoration: none;">
+                        <button type="button" class="btn">book now</button>
+                    </a>
+                </div>
+            </article>
+
+
+            <article class="room">
+                <div class="room-image">
+                    <img src="assets/accomodation/room3.jpg" alt="room image">
+                </div>
+                <div class="room-text"><br>
+                    <h3>Luxury Rooms</h3>
+                    <ul>
+                        <li>
+                            <i class="fas fa-arrow-alt-circle-right"></i>
+                            Lorem ipsum dolor sit amet.
+                        </li>
+                        <li>
+                            <i class="fas fa-arrow-alt-circle-right"></i>
+                            Lorem ipsum dolor sit amet.
+                        </li>
+                        <li>
+                            <i class="fas fa-arrow-alt-circle-right"></i>
+                            Lorem ipsum dolor sit amet.
+                        </li>
+                    </ul>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus exercitationem repellendus maxime ullam tempore architecto provident unde expedita quam beatae, dolore eligendi molestias sint tenetur incidunt voluptas. Unde corporis qui iusto vitae. Aut nesciunt id iste, cum esse commodi nemo?</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla corporis quasi officiis cumque, fugiat nostrum sunt, tempora animi dicta laborum beatae ratione doloremque. Delectus odio sit eius labore, atque quo?</p>
+                    <br>
+                    <a href="BookNow.php" style="text-decoration: none;">
+                        <button type="button" class="btn">book now</button>
+                    </a>
+                </div>
+            </article>
+
+        </div>
+    </section>
+
+
+    <section class="services sec-width" id="services">
+        <div class="title">
+            <h2>Services</h2>
+        </div>
+        <div class="services-container">
+
+            <article class="service">
+                <div class="service-icon">
+                    <span>
+                        <i class="fas fa-utensils"></i>
+                    </span>
+                </div>
+                <div class="service-content">
+                    <h2>Food Service</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias blanditiis tempore officia accusamus asperiores. Illum maxime eligendi necessitatibus laudantium iste nisi pariatur doloremque ut illo similique voluptatum enim distinctio perferendis, ad ipsam aspernatur omnis rem autem ex, reiciendis corporis suscipit!</p>
+                    <button type="button" class="btn">Know More</button>
+
+                </div>
+            </article>
+
+
+            <article class="service">
+                <div class="service-icon">
+                    <span>
+                        <i class="fas fa-swimming-pool"></i>
+                    </span>
+                </div>
+                <div class="service-content">
+                    <h2>Refreshment</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias blanditiis tempore officia accusamus asperiores. Illum maxime eligendi necessitatibus laudantium iste nisi pariatur doloremque ut illo similique voluptatum enim distinctio perferendis, ad ipsam aspernatur omnis rem autem ex, reiciendis corporis suscipit!</p>
+                    <button type="button" class="btn">Know More</button>
+                </div>
+            </article>
+            <article class="service">
+                <div class="service-icon">
+                    <span>
+                        <i class="fas fa-broom"></i>
+                    </span>
+                </div>
+                <div class="service-content">
+                    <h2>Housekeeping</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias blanditiis tempore officia accusamus asperiores. Illum maxime eligendi necessitatibus laudantium iste nisi pariatur doloremque ut illo similique voluptatum enim distinctio perferendis, ad ipsam aspernatur omnis rem autem ex, reiciendis corporis suscipit!</p>
+                    <button type="button" class="btn">Know More</button>
+                </div>
+            </article>
+
+            <article class="service">
+                <div class="service-icon">
+                    <span>
+                        <i class="fas fa-door-closed"></i>
+                    </span>
+                </div>
+                <div class="service-content">
+                    <h2>Room Security</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias blanditiis tempore officia accusamus asperiores. Illum maxime eligendi necessitatibus laudantium iste nisi pariatur doloremque ut illo similique voluptatum enim distinctio perferendis, ad ipsam aspernatur omnis rem autem ex, reiciendis corporis suscipit!</p>
+                    <button type="button" class="btn">Know More</button>
+                </div>
+            </article>
+
+        </div>
+    </section>
 </body>
+
 </html>
--- AdminPage.php
+++ AdminPage.php
@@ -4,125 +4,128 @@
 
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <link href="" rel="icon" sizes="16x16">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <title>AdminPage | The Grand Sweden Hotel</title>
-    <link rel="stylesheet" href="css/admin2.css" type="text/css"/>
-  </head>
-  <body>
-  
-<div class="container" >
-<h1>Admin Dashboard</h1>
-  <table class="rwd-table" style="overflow-x:auto;">
-    <tbody>
-      <tr>
-        <th></th>
-        <th>Edit</th>
-        <th>View</th>
-      </tr>
-      <tr>
-        <td data-th="Supplier Code">
-		Booking
-        </td>
-        <td data-th="Edit">  <a href="#">
-          <span class="glyphicon glyphicon-edit"></span>
-        </a>
-        </td>
-        <td data-th="View">
-		 <a href="AdminBookings.php">
-           <span class="glyphicon glyphicon-eye-open"></span>
-        </a>
-        </td>
-      </tr>
-      <tr>
-        <td data-th="Supplier Code">
-		Accomodation
-        </td>
-        <td data-th="Edit">  <a href="#">
-          <span class="glyphicon glyphicon-edit"></span>
-        </a>
-        </td>
-        <td data-th="View">
-		 <a href="Accommodations.php">
-           <span class="glyphicon glyphicon-eye-open"></span>
-        </a>
-        </td>
-      </tr>
-	  <tr>
-        <td data-th="Supplier Code">
-		Gallery
-        </td>
-        <td data-th="Edit">  <a href="#">
-          <span class="glyphicon glyphicon-edit"></span>
-        </a>
-        </td>
-        <td data-th="View">
-		 <a href="Gallery.php">
-           <span class="glyphicon glyphicon-eye-open"></span>
-        </a>
-        </td>
-      </tr>
-	  <tr>
-        <td data-th="Supplier Code">
-		Destination
-        </td>
-        <td data-th="Edit">  <a href="#">
-          <span class="glyphicon glyphicon-edit"></span>
-        </a>
-        </td>
-        <td data-th="View">
-		 <a href="Destination.php">
-           <span class="glyphicon glyphicon-eye-open"></span>
-        </a>
-        </td>
-      </tr>
-	  <tr>
-        <td data-th="Supplier Code">
-		Amenities
-        </td>
-        <td data-th="Edit">  <a href="#">
-          <span class="glyphicon glyphicon-edit"></span>
-        </a>
-        </td>
-        <td data-th="View">
-		 <a href="Amenities.php">
-           <span class="glyphicon glyphicon-eye-open"></span>
-        </a>
-        </td>
-      </tr>
-	  <tr>
-        <td data-th="Supplier Code">
-		Experiences
-        </td>
-        <td data-th="Edit">  <a href="#">
-          <span class="glyphicon glyphicon-edit"></span>
-        </a>
-        </td>
-        <td data-th="View">
-		 <a href="Experience.php">
-           <span class="glyphicon glyphicon-eye-open"></span>
-        </a>
-        </td>
-      </tr>
-	  <tr>
-        <td data-th="Supplier Code">
-		Gallery
-        </td>
-        <td data-th="Edit">  <a href="#">
-          <span class="glyphicon glyphicon-edit"></span>
-        </a>
-        </td>
-        <td data-th="View">
-		 <a href="Gallery.php">
-           <span class="glyphicon glyphicon-eye-open"></span>
-        </a>
-        </td>
-      </tr>
-    </tbody>
-  </table>
-</div>
+
+<head>
+  <link href="" rel="icon" sizes="16x16">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <title>AdminPage | The Grand Sweden Hotel</title>
+  <link rel="stylesheet" href="css/admin2.css" type="text/css" />
+</head>
+
+<body>
+
+  <div class="container">
+    <h1>Admin Dashboard</h1>
+    <table class="rwd-table" style="overflow-x:auto;">
+      <tbody>
+        <tr>
+          <th></th>
+          <th>Edit</th>
+          <th>View</th>
+        </tr>
+        <tr>
+          <td data-th="Supplier Code">
+            Booking
+          </td>
+          <td data-th="Edit"> <a href="#">
+              <span class="glyphicon glyphicon-edit"></span>
+            </a>
+          </td>
+          <td data-th="View">
+            <a href="AdminBookings.php">
+              <span class="glyphicon glyphicon-eye-open"></span>
+            </a>
+          </td>
+        </tr>
+        <tr>
+          <td data-th="Supplier Code">
+            Accomodation
+          </td>
+          <td data-th="Edit"> <a href="#">
+              <span class="glyphicon glyphicon-edit"></span>
+            </a>
+          </td>
+          <td data-th="View">
+            <a href="Accommodations.php">
+              <span class="glyphicon glyphicon-eye-open"></span>
+            </a>
+          </td>
+        </tr>
+        <tr>
+          <td data-th="Supplier Code">
+            Gallery
+          </td>
+          <td data-th="Edit"> <a href="#">
+              <span class="glyphicon glyphicon-edit"></span>
+            </a>
+          </td>
+          <td data-th="View">
+            <a href="Gallery.php">
+              <span class="glyphicon glyphicon-eye-open"></span>
+            </a>
+          </td>
+        </tr>
+        <tr>
+          <td data-th="Supplier Code">
+            Destination
+          </td>
+          <td data-th="Edit"> <a href="#">
+              <span class="glyphicon glyphicon-edit"></span>
+            </a>
+          </td>
+          <td data-th="View">
+            <a href="Destination.php">
+              <span class="glyphicon glyphicon-eye-open"></span>
+            </a>
+          </td>
+        </tr>
+        <tr>
+          <td data-th="Supplier Code">
+            Amenities
+          </td>
+          <td data-th="Edit"> <a href="#">
+              <span class="glyphicon glyphicon-edit"></span>
+            </a>
+          </td>
+          <td data-th="View">
+            <a href="Amenities.php">
+              <span class="glyphicon glyphicon-eye-open"></span>
+            </a>
+          </td>
+        </tr>
+        <tr>
+          <td data-th="Supplier Code">
+            Experiences
+          </td>
+          <td data-th="Edit"> <a href="#">
+              <span class="glyphicon glyphicon-edit"></span>
+            </a>
+          </td>
+          <td data-th="View">
+            <a href="Experience.php">
+              <span class="glyphicon glyphicon-eye-open"></span>
+            </a>
+          </td>
+        </tr>
+        <tr>
+          <td data-th="Supplier Code">
+            Gallery
+          </td>
+          <td data-th="Edit"> <a href="#">
+              <span class="glyphicon glyphicon-edit"></span>
+            </a>
+          </td>
+          <td data-th="View">
+            <a href="Gallery.php">
+              <span class="glyphicon glyphicon-eye-open"></span>
+            </a>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </body>
+
 </html>
