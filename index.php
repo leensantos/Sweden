f704d9cf8855f7d28f80527d9e3930cddbe853a6
cjariola <ariola.cydel@gmail.com>
2021-02-12 18:24:28 +0800

    masarap kwekkwek

--- index.php
+++ index.php
@@ -5,47 +5,49 @@
 <html lang="en">
 
 <head>
-  <link rel="stylesheet" href="css/index-styles.css">
-  <title>Home | The Grand Sweden Hotel</title>
+    <link rel="stylesheet" href="css/index-styles.css">
+    <title>Home | The Grand Sweden Hotel</title>
 </head>
 
 <body>
-  <!-- Video Background -->
-  <video autoplay muted loop id="myVideo" clas="fullscreen-bg">
-    <source src="assets/Home.mp4" type="video/mp4">
-    Your browser does not support HTML5 video.
-  </video>
-  <section class="index-top">
-    <!-- Navigation Bar -->
-    <nav>
-      <div class="logo">GRAND SWEDEN HOTEL</div>
-      <ul class="links">
-        <li><a href="index.php">Home</a></li>
-        <li><a href="Accommodations.php">Accommodations</a></li>
-        <li><a href="Experiences.php">Experiences</a></li>
-        <li><a href="Amenities.php">Amenities</a></li>
-        <li><a href="Offers.php">Offers</a></li>
-        <li><a href="Gallery.php">Gallery</a></li>
-        <li><a href="Destination.php">Destination</a></li>
-        <li><a href="ContactUs.php">Contact Us</a></li>
-        <li><a href="BookNow.php" class="btn">Book Now</a></li>
-      </ul>
-      <label for="nav-toggle" class="icon-burger">
-        <div class="line"></div>
-        <div class="line"></div>
-        <div class="line"></div>
-      </label>
-    </nav>
-    <!-- Title -->
-    <div class="title">
-      <h1>The Grand <br /> Sweden Hotel</h1>
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium, expedita fugiat perferendis ducimus
-        odio quo, cumque quae at eveniet, sequi est in officia natus? Suscipit veritatis voluptate saepe earum
-        quod!<br /><br />
-        <a href="SignIn.php" class="btn">Sign In</a>
-      </p>
-    </div>
-  </section>
+    <!-- Video Background -->
+    <video autoplay muted loop id="myVideo" clas="fullscreen-bg">
+        <source src="assets/Home.mp4" type="video/mp4">
+        Your browser does not support HTML5 video.
+    </video>
+    <section class="index-top">
+        <!-- Navigation Bar -->
+        <nav>
+            <input id="nav-toggle" type="checkbox">
+            <div class="logo">GRAND SWEDEN HOTEL</div>
+            <ul class="links">
+                <li><a href="index.php">Home</a></li>
+                <li><a href="Accommodations.php">Accommodations</a></li>
+                <li><a href="Experiences.php">Experiences</a></li>
+                <li><a href="Amenities.php">Amenities</a></li>
+                <li><a href="Offers.php">Offers</a></li>
+                <li><a href="Gallery.php">Gallery</a></li>
+                <li><a href="Destination.php">Destination</a></li>
+                <li><a href="ContactUs.php">Contact Us</a></li>
+                <li><a href="BookNow.php" class="btn">Book Now</a></li>
+            </ul>
+            <label for="nav-toggle" class="icon-burger">
+                <div class="line"></div>
+                <div class="line"></div>
+                <div class="line"></div>
+            </label>
+        </nav>
+        <!-- Title -->
+        <div class="title">
+            <h1>The Grand <br /> Sweden Hotel</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium, expedita fugiat perferendis
+                ducimus
+                odio quo, cumque quae at eveniet, sequi est in officia natus? Suscipit veritatis voluptate saepe earum
+                quod!<br /><br />
+                <a href="SignIn.php" class="btn">Sign In</a>
+            </p>
+        </div>
+    </section>
 </body>
 
 </html>
