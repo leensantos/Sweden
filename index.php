77cc84aedee33e18837c2250608252429b071589
leensantos <me4299@gmail.com>
2021-02-14 01:45:09 +0800

    awimbawe

--- index.php
+++ index.php
@@ -5,48 +5,91 @@
 <html lang="en">
 
 <head>
-    <link rel="stylesheet" href="css/index-styles.css">
-    <title>Home | The Grand Sweden Hotel</title>
+  <link rel="stylesheet" href="css/index-styles.css">
+  <title>Home | The Grand Sweden Hotel</title>
 </head>
 
 <body>
-    <!-- Video Background -->
-    <video autoplay muted loop id="myVideo" clas="fullscreen-bg">
-        <source src="assets/Home.mp4" type="video/mp4">
-        Your browser does not support HTML5 video.
-    </video>
-    <section class="index-top">
-        <!-- Navigation Bar -->
-        <nav>
-            <input id="nav-toggle" type="checkbox">
-            <div class="logo">GRAND SWEDEN HOTEL</div>
-            <ul class="links">
-                <li><a href="index.php">Home</a></li>
-                <li><a href="Accommodations.php">Accommodations</a></li>
-                <li><a href="Amenities.php">Amenities</a></li>
-                <li><a href="Offers.php">Offers</a></li>
-                <li><a href="Gallery.php">Gallery</a></li>
-                <li><a href="Destination.php">Destination</a></li>
-                <li><a href="ContactUs.php">Contact Us</a></li>
-                <li><a href="BookNow.php" class="btn">Book Now</a></li>
-            </ul>
-            <label for="nav-toggle" class="icon-burger">
-                <div class="line"></div>
-                <div class="line"></div>
-                <div class="line"></div>
-            </label>
-        </nav>
-        <!-- Title -->
-        <div class="title">
-            <h1>The Grand <br /> Sweden Hotel</h1>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium, expedita fugiat perferendis
-                ducimus
-                odio quo, cumque quae at eveniet, sequi est in officia natus? Suscipit veritatis voluptate saepe earum
-                quod!<br /><br />
-                <a href="SignIn.php" class="btn">Sign In</a>
-            </p>
-        </div>
-    </section>
+  <!-- Video Background -->
+  <video autoplay muted loop id="myVideo" clas="fullscreen-bg">
+    <source src="assets/Home.mp4" type="video/mp4">
+    Your browser does not support HTML5 video.
+  </video>
+  <section class="index-top">
+    <!-- Navigation Bar -->
+    <nav>
+      <input id="nav-toggle" type="checkbox">
+      <div class="logo">GRAND SWEDEN HOTEL</div>
+      <ul class="links">
+        <li><a href="index.php">Home</a></li>
+        <li><a href="Accommodations.php">Accommodations</a></li>
+        <li><a href="Amenities.php">Amenities</a></li>
+        <li><a href="Offers.php">Offers</a></li>
+        <li><a href="Gallery.php">Gallery</a></li>
+        <li><a href="Destination.php">Destination</a></li>
+        <li><a href="ContactUs.php">Contact Us</a></li>
+        <li><a href="BookNow.php" class="btn">Book Now</a></li>
+      </ul>
+      <label for="nav-toggle" class="icon-burger">
+        <div class="line"></div>
+        <div class="line"></div>
+        <div class="line"></div>
+      </label>
+    </nav>
+    <!-- Title -->
+    <div class="title">
+      <h1>happiness is what we sell.. <small>subject to change</small></h1>
+      <div class="container">
+        <forms action="">
+          <div class="container-1">
+            <div class="input-group">
+              <label for="check-in" class="input-label">Check in</label>
+              <input type="date" class="input" id="check-in" name="check-in" required>
+            </div>
+            <div class="input-group">
+              <label for="check-out" class="input-label">Check out</label>
+              <input type="date" " class=" input" id="check-out" name="check-out" required>
+            </div>
+            <div class="input-group">
+              <label for="adults" class="input-label">Adults</label>
+              <select class="options" id="adults" name="adults" required>
+                <option disabled selected>0</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+                <option value="10">10</option>
+              </select>
+            </div>
+            <div class="input-group">
+              <label for="children" class="input-label">Children</label>
+              <select class="options" id="children" name="children" required>
+                <option disabled selected>0</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+                <option value="10">10</option>
+              </select>
+            </div>
+            <div class="input-group">
+              <input type="submit" value="Book Now" class="register" />
+            </div>
+          </div>
+        </forms>
+      </div>
+    </div>
+  </section>
 </body>
 
 </html>
