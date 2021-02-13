b5750d80365c1d3d4da027eb3d52e07de3379baa
cjariola <ariola.cydel@gmail.com>
2021-02-13 20:52:51 +0800

    user profile

--- UserProfile.php
+++ UserProfile.php
@@ -1,107 +1,40 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" href="css/navigation-bar.css">
-    <link rel="stylesheet" type="text/css" href="css/user.css">
-    <link rel="stylesheet" type="text/css" href="css/amenities.css">
-    <link rel="stylesheet" type="text/css" href="css/footer.css">
-    <link rel="stylesheet" type="text/css"
-        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
-    <title>Contact Us | The Grand Sweden Hotel</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" />
+    <link rel="stylesheet" href="css/user.css">
 </head>
 
 <body>
-    <!-- Nagigation bar -->
-    <nav>
-        <input id="nav-toggle" type="checkbox">
-        <div class="logo">GRAND SWEDEN HOTEL</div>
-        <ul class="links">
-            <li><a href="index.php">Home</a></li>
-            <li><a href="Accommodations.php">Accommodations</a></li>
-            <li><a href="Amenities.php">Amenities</a></li>
-            <li><a href="Offers.php">Offers</a></li>
-            <li><a href="Gallery.php">Gallery</a></li>
-            <li><a href="Destination.php">Destination</a></li>
-            <li><a href="ContactUs.php">Contact Us</a></li>
-            <li><a href="BookNow.php" class="btn">Book Now</a></li>
-        </ul>
-        <label for="nav-toggle" class="icon-burger">
-            <div class="line"></div>
-            <div class="line"></div>
-            <div class="line"></div>
-        </label>
-    </nav>
-
-
-    <!-- Contact US -->
-    <div class="contact-us">
-        <div class="contact-wrap">
-            <div class="contact-in">
-                <h1>ADMINISTRATOR CONTROL PANEL</h1>
-
-                <a href="adminbookings.php" class="button">BOOKINGS</a>
-                <a href="adminpage.php" class="button">CONTACT US</a>
-
-
-
+    <div class="profile-card">
+        <div class="card-header">
+            <div class="pic">
+                <img src="assets/admin.png" alt="">
             </div>
-            <div class="contact-in">
-                <h1>OTHERS</h1>
-                <a href="adminbookings.php" class="button">ADD NEW ADMIN</a>
-                <a href="adminpage.php" class="button">LOGOUT</a>
-
-                <div class="contact-in">
-                    <iframe
-                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d224345.83923192776!2d77.06889754725782!3d28.52758200617607!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390cfd5b347eb62d%3A0x52c2b7494e204dce!2sNew%20Delhi%2C%20Delhi!5e0!3m2!1sen!2sin!4v1601968196548!5m2!1sen!2sin"
-                        width="100%" height="auto" frameborder="0" style="border:0;" allowfullscreen=""
-                        aria-hidden="false" tabindex="0"></iframe>
+            <div class="name">John Doe</div>
+            <div class="desc">Administrator</div>
+            <div class="sm">
+                <a href="facebook.com" class="fab fa-facebook-f"></a>
+                <a href="twitter.com" class="fab fa-twitter"></a>
+                <a href="github.com" class="fab fa-github"></a>
+                <a href="youtube.com" class="fab fa-youtube"></a>
+            </div>
+            <a href="index.php" class="contact-btn">logout</a>
+        </div>
+        <div class="card-footer">
+            <div class="numbers">
+                <div class="item">
+                    <a href="signin.php" class="admin-btn">Add New Admin</a> <br>
+                    <a href="adminpage.php" class="admin-btn">View Contacts</a> <br>
+                    <a href="adminbookings.php" class="admin-btn">View Bookings</a>
                 </div>
             </div>
         </div>
     </div>
-    <!-- Footer -->
-    <footer>
-        <div class="container">
-            <div class="row">
-                <div class="footer-col col-1">
-                    <h4>Grand Sweden Hotel</h4>
-                    <ul>
-                        <li><a href="index.php">Home</a></li>
-                        <li><a href="accomoations.php">Accomodation</a></li>
-                        <li><a href="experiences.php">Experiences</a></li>
-                        <li><a href="offers.php">Offers</a></li>
-                        <li><a href="gallery.php">Gallery</a></li>
-                        <li><a href="destination.php">Destination</a></li>
-                        <li><a href="contactus.php">Contact Us</a></li>
-                    </ul>
-                </div>
-                <div class="footer-col col-2">
-                    <h4>Get in touch</h4>
-                    <ul>
-                        <li>PO Box 222 333 Sen. Gil J. Puyat Ave, Makati</li>
-                        <li>+632 8247-5000</li>
-                        <li>Privacy Policy</li>
-                    </ul>
-                </div>
-                <div class="footer-col col-3">
-                    <h4>follow us</h4>
-                    <div class="social-links">
-                        <a href="#"><i class="fab fa-facebook-f"></i></a>
-                        <a href="#"><i class="fab fa-twitter"></i></a>
-                        <a href="#"><i class="fab fa-instagram"></i></a>
-                        <a href="#"><i class="fab fa-youtube"></i></a>
-                    </div>
-                </div>
-            </div>
-            <div class="copyright">
-                <a href="signin.php">Administrator Login</a><br>
-                <a>© Copyright 2021. All Rights Reserved.</a>
-            </div>
-        </div>
-    </footer>
 </body>
 
 </html>
