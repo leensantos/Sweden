062652377d7c2dac68ccc7f4c4cd137d32afc04b
leensantos <me4299@gmail.com>
2021-02-14 05:57:53 +0800

    bork bork bork

--- AdminBookings.php
+++ AdminBookings.php
@@ -6,54 +6,104 @@
 <html>
 
 <head>
-  <link href="" rel="icon" sizes="16x16">
+  <!-- <link href="" rel="icon" sizes="16x16"> -->
   <link href="css/admin1.css" rel="stylesheet" type="text/css">
-  <title>AdminBookings | The Grand Sweden Hotel</title>
+  <link rel="stylesheet" type="text/css" href="css/navigation-bar.css">
+  <link rel="stylesheet" type="text/css" href="css/footer.css">
+  <title>Admin Bookings | The Grand Sweden Hotel</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> -->
 </head>
 
 <body>
+  <!-- Nagigation bar -->
+  <nav>
+    <div class="logo"><a href="index.php">The Grand Sweden Hotel</a></div>
+    <ul class="links">
+      <li><a href="adminpage.php">Contact Us?</a></li>
+      <li><a href="adminbookings.php">Bookings</a></li>
+      <li><a href="userprofile.php">Profile?</a></li>
+      <li><a href="logout.php">Logout</a></li>
+    </ul>
+    <label for="nav-toggle" class="icon-burger">
+      <div class="line"></div>
+      <div class="line"></div>
+      <div class="line"></div>
+    </label>
+  </nav>
 
-  <div class="container">
-    <h1>Hotel Reservations</h1>
-    <table class="rwd-table">
-      <tbody>
-        <tr>
-          <th>Name</th>
-          <th>Check-in</th>
-          <th>Check-out</th>
-          <th>Adult</th>
-          <th>Children</th>
-          <th>Price</th>
-        </tr>
+  <!-- Page Content -->
+  <div class="page-content">
 
-        <!-- SAMPLE RESULT --->
-        <tr>
-          <td data-th="Name">
-            Dea
-          </td>
-          <td data-th="Check-in">
-            xxx
-          </td>
-          <td data-th="Check-out">
-            xxx
-          </td>
-          <td data-th="Adult">
-            xxx
-          </td>
-          <td data-th="Children">
-            xxx
-          </td>
-          <td data-th="Price">
-            xxx
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <div class="container">
+      <h1>Hotel Reservations</h1>
+      <table class="table">
+        <thead class="thead-light">
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Check in</th>
+            <th scope="col">Check out</th>
+            <th scope="col">Adult</th>
+            <th scope="col">Children</th>
+            <th scope="col">Room</th>
+            <th scope="col">More</th>
+            <th scope="col">More</th>
+            <th scope="col">More</th>
+            <th scope="col">Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th data-title="ID" scope="row">1</th>
+            <td data-title="Check in">weeeeee</td>
+            <td data-title="Check out">weeeeee</td>
+            <td data-title="Adult">weeeeee</td>
+            <td data-title="Children">weeeeee</td>
+            <td data-title="Room">weeeeee</td>
+            <td data-title="More">weeeeee</td>
+            <td data-title="More">weeeeee</td>
+            <td data-title="More">weeeeee</td>
+            <td data-title="Price">weeeeee</td>
+          </tr>
+          <tr>
+            <th data-title="ID" scope="row">2</th>
+            <td data-title="Check in">weeeeee</td>
+            <td data-title="Check out">weeeeee</td>
+            <td data-title="Adult">weeeeee</td>
+            <td data-title="Children">weeeeee</td>
+            <td data-title="Room">weeeeee</td>
+            <td data-title="More">weeeeee</td>
+            <td data-title="More">weeeeee</td>
+            <td data-title="More">weeeeee</td>
+            <td data-title="Price">weeeeee</td>
+          </tr>
+          <tr>
+            <th data-title="ID" scope="row">3</th>
+            <td data-title="Check in">weeeeee</td>
+            <td data-title="Check out">weeeeee</td>
+            <td data-title="Adult">weeeeee</td>
+            <td data-title="Children">weeeeee</td>
+            <td data-title="Room">weeeeee</td>
+            <td data-title="More">weeeeee</td>
+            <td data-title="More">weeeeee</td>
+            <td data-title="More">weeeeee</td>
+            <td data-title="Price">weeeeee</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+
   </div>
+  <!-- Footer -->
+  <!-- <footer>
+    <div class="footer-container">
+      <div class="copyright">
+        <a>© Copyright 2021. All Rights Reserved.</a>
+      </div>
+    </div>
+  </footer> -->
 </body>
 
 </html>
--- AdminPage.php
+++ AdminPage.php
@@ -6,54 +6,84 @@
 <html>
 
 <head>
-    <link href="" rel="icon" sizes="16x16">
-    <link href="css/admin1.css" rel="stylesheet" type="text/css">
-    <title>AdminBookings | The Grand Sweden Hotel</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <!-- <link href="" rel="icon" sizes="16x16"> -->
+  <link href="css/admin1.css" rel="stylesheet" type="text/css">
+  <link rel="stylesheet" type="text/css" href="css/navigation-bar.css">
+  <link rel="stylesheet" type="text/css" href="css/footer.css">
+  <title>Admin Bookings | The Grand Sweden Hotel</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> -->
 </head>
 
 <body>
+  <!-- Nagigation bar -->
+  <nav>
+    <div class="logo"><a href="index.php">The Grand Sweden Hotel</a></div>
+    <ul class="links">
+      <li><a href="adminpage.php">Contact Us?</a></li>
+      <li><a href="adminbookings.php">Bookings</a></li>
+      <li><a href="userprofile.php">Profile?</a></li>
+      <li><a href="logout.php">Logout</a></li>
+    </ul>
+    <label for="nav-toggle" class="icon-burger">
+      <div class="line"></div>
+      <div class="line"></div>
+      <div class="line"></div>
+    </label>
+  </nav>
+
+  <!-- Page Content -->
+  <div class="page-content">
 
     <div class="container">
-        <h1>CONTACT US</h1>
-        <table class="rwd-table">
-            <tbody>
-                <tr>
-                    <th>Name</th>
-                    <th>Check-in</th>
-                    <th>Check-out</th>
-                    <th>Adult</th>
-                    <th>Children</th>
-                    <th>Price</th>
-                </tr>
-
-                <!-- SAMPLE RESULT --->
-                <tr>
-                    <td data-th="Name">
-                        Dea
-                    </td>
-                    <td data-th="Check-in">
-                        xxx
-                    </td>
-                    <td data-th="Check-out">
-                        xxx
-                    </td>
-                    <td data-th="Adult">
-                        xxx
-                    </td>
-                    <td data-th="Children">
-                        xxx
-                    </td>
-                    <td data-th="Price">
-                        xxx
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+      <h1>Contact Us</h1>
+      <table class="table">
+        <thead class="thead-light">
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Full Name</th>
+            <th scope="col">Email Address</th>
+            <th scope="col">Subject</th>
+            <th scope="col">Message</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th data-title="ID" scope="row">1</th>
+            <td data-title="Full Name">weeeeee</td>
+            <td data-title="Email Address">weeeeee</td>
+            <td data-title="Subject">weeeeee</td>
+            <td data-title="Message">weeeeee</td>
+          </tr>
+          <tr>
+            <th data-title="ID" scope="row">2</th>
+            <td data-title="Full Name">weeeeee</td>
+            <td data-title="Email Address">weeeeee</td>
+            <td data-title="Subject">weeeeee</td>
+            <td data-title="Message">weeeeee</td>
+          </tr>
+          <tr>
+            <th data-title="ID" scope="row">3</th>
+            <td data-title="Full Name">weeeeee</td>
+            <td data-title="Email Address">weeeeee</td>
+            <td data-title="Subject">weeeeee</td>
+            <td data-title="Message">weeeeee</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
+
+  </div>
+  <!-- Footer -->
+  <!-- <footer>
+    <div class="footer-container">
+      <div class="copyright">
+        <a>© Copyright 2021. All Rights Reserved.</a>
+      </div>
+    </div>
+  </footer> -->
 </body>
 
 </html>
