71668470186008f46bc4dbb6e1777f4d8f883bf8
[junmar0117] <[marjungil17@gmail.com]>
2021-02-14 20:33:55 +0800

    add required

--- ContactUs.php
+++ ContactUs.php
@@ -39,11 +39,11 @@
       <div class="contact-in">
         <h1>Send a Message</h1>
         <form action="SendMessage.php">
-          <input type="text" class="contact-in-input" id="fName" name="fullname" placeholder="Full Name">
+          <input type="text" class="contact-in-input" id="fName" name="fullname" placeholder="Full Name" required>
           <input type="email" class="contact-in-input" id="emailAdd" placeholder="Email Address (name@mail.com)"
-            pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$">
-          <input type="text" class="contact-in-input" id="subject" name="subject" placeholder="Subject">
-          <textarea placeholder="Message" id ="content" class="contact-in-textarea"></textarea>
+            pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" required>
+          <input type="text" class="contact-in-input" id="subject" name="subject" placeholder="Subject" required>
+          <textarea placeholder="Message" id ="content" class="contact-in-textarea" required></textarea>
           <input type="submit" value="SUBMIT" class="contact-in-btn">
         </form>
       </div>
