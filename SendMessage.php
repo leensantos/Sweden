23e3d058e3aef000b49df4657bbd122353f85104
[junmar0117] <[marjungil17@gmail.com]>
2021-02-14 21:45:37 +0800

    add date and time to messages

--- SendMessage.php
+++ SendMessage.php
@@ -3,8 +3,9 @@ session_start(); //starts the session
 
 if($_SERVER['REQUEST_METHOD'] = "POST") //Added an if to keep the page secured
 {
+    date_default_timezone_set('Asia/Manila');
     $fullname = ($_POST['fullname']);
-    $time = strftime("%X");//time
+    $time = strftime("%r");//time
     $date = strftime("%B %d, %Y");//date
     $emailAdd = ($_POST['emailAdd']);
     $subject = ($_POST['subject']);
@@ -12,7 +13,7 @@ if($_SERVER['REQUEST_METHOD'] = "POST") //Added an if to keep the page secured
     $con = mysqli_connect("localhost", "root", "", "swedendb") or die(mysqli_error()); //Connect to server
 
     
-    mysqli_query($con, "INSERT INTO messages (fName, emailAdd, date, time subject, content) VALUES
+    mysqli_query($con, "INSERT INTO messages (fName, emailAdd, date, time, subject, content) VALUES
     ('$fullname','$emailAdd','$date','$time','$subject','$content')"); //SQL query
     header("location: ContactUs.php");
 }
