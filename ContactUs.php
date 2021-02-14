80717841b41a47803a91333e45ea36d5a10a4dfe
[junmar0117] <[marjungil17@gmail.com]>
2021-02-14 20:21:33 +0800

    session verif

--- ContactUs.php
+++ ContactUs.php
@@ -1,3 +1,11 @@
+<?php
+session_start(); //starts the session
+if (!$_SESSION['user']) { //checks if user is not logged in
+  header("location:index.php"); // redirects if user is not logged in
+}
+$user = $_SESSION['user']; //assigns user value
+?>
+
 <!DOCTYPE html>
 <html>
 
