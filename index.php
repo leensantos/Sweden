db7bcdb028e4d0fb3df7e823f14b000e20455bc3
leensantos <me4299@gmail.com>
2021-02-16 03:06:25 +0800

    byebyebye

--- index.php
+++ index.php
@@ -37,58 +37,6 @@
         <div class="line"></div>
       </label>
     </nav>
-    <!-- Title -->
-    <div class="title">
-      <div class="container">
-        <form action="BookNow.php" method="get">
-          <div class="container-1">
-            <div class="input-group">
-              <label for="check-in" class="input-label">Check in</label>
-              <input type="date" class="input" id="check-in" name="check-in" required>
-            </div>
-            <div class="input-group">
-              <label for="check-out" class="input-label">Check out</label>
-              <input type="date" " class=" input" id="check-out" name="check-out" required>
-            </div>
-            <div class="input-group">
-              <label for="adults" class="input-label">Adults</label>
-              <select class="options" id="adults" name="adults" required>
-                <option disabled selected>0</option>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-                <option value="6">6</option>
-                <option value="7">7</option>
-                <option value="8">8</option>
-                <option value="9">9</option>
-                <option value="10">10</option>
-              </select>
-            </div>
-            <div class="input-group">
-              <label for="children" class="input-label">Children</label>
-              <select class="options" id="children" name="children" required>
-                <option disabled selected>0</option>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-                <option value="6">6</option>
-                <option value="7">7</option>
-                <option value="8">8</option>
-                <option value="9">9</option>
-                <option value="10">10</option>
-              </select>
-            </div>
-            <div class="input-group">
-              <input type="submit" value="Book Now" class="register" />
-            </div>
-          </div>
-        </form>
-      </div>
-    </div>
   </section>
 </body>
 
