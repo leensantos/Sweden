80f96cdcb6a4cd9152d9a398de44ee9601063740
kuyaeron <abellon.aaron@gmail.com>
2021-02-15 15:00:37 +0800

    mysqli_connect_error()

--- connection.php
+++ connection.php
@@ -3,5 +3,5 @@ $db_name = "swedendb";
 $db_username = "root";
 $db_pass = "";
 $db_host = "localhost";
-$con = mysqli_connect("$db_host","$db_username","$db_pass", "$db_name") or die(mysqli_error()); //Connect to server
+$con = mysqli_connect("$db_host","$db_username","$db_pass", "$db_name") or die(mysqli_connect_error()); //Connect to server
 ?>
